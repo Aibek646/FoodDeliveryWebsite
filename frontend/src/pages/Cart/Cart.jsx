1007c7c33094e7c3fc05d68ed24c69be69d61b72
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2025-06-18 13:11:00 +0500

    some functionalities

--- frontend/src/pages/Cart/Cart.jsx
+++ frontend/src/pages/Cart/Cart.jsx
@@ -18,11 +18,11 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map(() => {
-          if (cartItems[cartItems._id] > 0) {
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
             return (
               <div className="cart-items-title cart-items-item">
-                <p>{cartItems.name}</p>
+                <p>{item.name}</p>
               </div>
             );
           }
