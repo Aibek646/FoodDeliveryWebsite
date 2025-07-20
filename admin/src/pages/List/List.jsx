d24ea1d0a612f3a3a6ddaebc84f648b923ffc827
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2025-07-20 15:34:34 +0500

    cart controller

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -5,16 +5,26 @@ import axious from "axios";
 import { toast } from "react-toastify";
 import { useEffect } from "react";
 
-const List = () => {
-  const url = "http://localhost:4000";
+const List = ({ url }) => {
   const [list, setList] = useState([]);
 
   const fetchList = async () => {
     const response = await axious.get(`${url}/api/food/list`);
-    console.log(response.data);
 
     if (response.data.success) {
       setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+
+  const removeFood = async (foodId) => {
+    const response = await axious.post(`${url}/api/food/remove`, {
+      id: foodId,
+    });
+    await fetchList();
+    if (response.data.success) {
+      toast.success(response.data.message);
     } else {
       toast.error("Error");
     }
@@ -42,7 +52,9 @@ const List = () => {
               <p>{item.name}</p>
               <p>{item.category}</p>
               <p>${item.price}</p>
-              <p>X</p>
+              <p onClick={() => removeFood(item._id)} className="cursor">
+                X
+              </p>
             </div>
           );
         })}
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import cors from "cors";
-
 import foodRouter from "./routes/foodRoute.js";
 import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
 
 // app config
 
@@ -19,6 +21,8 @@ connectDB();
 //api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images", express.static("uploads"));
+app.use("/api/user", userRouter);
+app.use("/api/cart", cartRouter);
 
 app.get("/", (req, res) => {
   res.send("API Working");
