bab75b436dc3368c347530e8ef7f27f2f56444e6
Aibek Ramazanov <aibekramazanov@Aibeks-MacBook-Pro.local>
2025-07-24 15:14:34 +0500

    place
    	modified:   backend/controllers/orderController.js

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -2,7 +2,6 @@ import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // olacing user order for frontend
 const placeOrder = async (req, res) => {
   const frontend_url = "http://localhost:5173";
